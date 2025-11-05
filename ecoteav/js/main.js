68246be39c83f29a76642efdbf8a4cf75cbeac55
rwl-creator <xxx696193@gmail.com>
2025-11-05 12:27:52 +0900

    1105

---
+++ ecoteav/js/main.js
@@ -0,0 +1,13 @@
+// header
+const header = document.querySelector('#header');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    header.classList.add('active');
+  } else {
+    header.classList.remove('active');
+  }
+});
+const main = document.querySelector('section');
+const headerHeight = header.offsetHeight;
+main.style.paddingTop = `${headerHeight}px`;
+
