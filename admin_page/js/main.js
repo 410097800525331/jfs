e7eb60b550e911be7ddd99efa3444f46d93014b5
rwl-creator <xxx696193@gmail.com>
2025-11-10 18:00:15 +0900

    1110

--- admin_page/js/main.js
+++
@@ -1,8 +0,0 @@
-const menuItems = document.querySelectorAll('.menu_item');
-
-menuItems.forEach(item => {
-  item.addEventListener('click', () => {
-    document.querySelector('.menu_item.active')?.classList.remove('active');
-    item.classList.add('active');
-  });
-});
