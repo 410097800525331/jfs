0831fd61e2a8ce7984c27ae6024797d831093b61
rwl-creator <xxx696193@gmail.com>
2025-11-21 17:11:57 +0900

    1121

--- mysite_bs/js/scrollMotuin.js
+++ mysite_bs/js/scrollMotuin.js
@@ -5,12 +5,12 @@ $(window).scroll(function () {
   const speed = 1000,
     easing = "easeOutExpo",
     pos = 0,
-    offset = 0.5;
+    offset = 2.5;
 
   // 오른쪽 상단에 스크롤 양과 창 높이 값 출력
   // console.log(windowScrollTop + " , " + windowHeight);
 
-  if (windowScrollTop > windowHeight / offset) {
+  if (windowScrollTop > windowHeight * offset) {
     //참일 경우
     // i는 0, 1, 2, 3
     for (let i = 0; i < 4; i++) {
@@ -29,4 +29,17 @@ $(window).scroll(function () {
       .stop()
       .animate({ top: pos + 600, opacity: 0 }, speed, easing);
   }
+
+  // 헤더 배경
+  // 창의 스크롤 양이 0보다 크면
+  if ($(window).scrollTop() > 0) {
+    $("#header").addClass("active");
+  } else {
+    $("#header").removeClass("active");
+  }
+  // if (windowScrollTop > 0) {
+  //   $("#header").addClass("active");
+  // } else {
+  //   $("#header").removeClass("active");
+  // }
 });
