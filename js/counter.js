4cdf2bca48810a93f42518367cccbd6f2baed3f4
rwl-creator <xxx696193@gmail.com>
2025-11-27 17:55:45 +0900

    1127

--- js/counter.js
+++ js/counter.js
@@ -1,5 +1,4 @@
 $(function () {
-
   function runCounter($counter) {
     var target = parseInt($counter.attr("data-target")),
       count = 0,
@@ -8,10 +7,8 @@ $(function () {
     $counter.text(0);
 
     var counterInterval = setInterval(function () {
-      // count += Math.ceil(target / 100);
       count++;
       if (count >= target) {
-        // count = target;
         clearInterval(counterInterval);
       }
       $counter.text(count);
@@ -20,13 +17,12 @@ $(function () {
 
   function checkScroll() {
     const scrollTop = $(window).scrollTop(),
-      windowHeight = $(window).height();
-    elementTop = $('#counters').offset().top;
+      windowHeight = $(window).height(),
+      elementTop = $('#counters').offset().top;
 
     $(".counter").each(function () {
       const $this = $(this);
 
-      // if (scrollTop + windowHeight > elementTop + 50 && scrollTop < elementTop + $this.outerHeight()) {
       if (scrollTop + windowHeight > elementTop + 100) {
         if (!$this.hasClass("active")) {
           $this.addClass("active");
@@ -37,9 +33,9 @@ $(function () {
         $this.text(0);
       }
     });
+    console.log($(window).scrollTop());
   }
 
-  // $(window).on("scroll", $.throttle(100, checkScroll));
-  $(window).on("scroll", checkScroll);
+  $(window).on("scroll", $.throttle(100, checkScroll));
   checkScroll();
 });
