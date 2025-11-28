c46c94a4ac949e69d6f44f1a761e3a7e1a9e2501
rwl-creator <xxx696193@gmail.com>
2025-11-28 09:43:30 +0900

    1128

--- js/scrollMotuin.js
+++ js/scrollMotuin.js
@@ -6,6 +6,9 @@ $(window).scroll(function () {
   $(".motion").each(function (i, el) {
     // position(): 포지션이 지정된 가장 가까운 조상 요소를 기준으로 지정한다.
     const elementTop = $(el).position().top;
+    // offset1: .motion의 움직이기 시작하는 시점
+    // -> 숫자가 클수록 더 많이 스크롤해야 동작한다.
+    // offset2: .motion의 시작 위치
     let offset1, offset2, speed;
 
     switch (i) {
@@ -18,27 +21,13 @@ $(window).scroll(function () {
       case 2:
         offset1 = 800;
         offset2 = 600;
-        speed = 400;
+        speed = 1000;
         break;
       default:
         offset1 = 1800;
         offset2 = 600;
         speed = 400;
     }
-
-    // if (i < 1) {
-    //   offset1 = 800;
-    //   offset2 = 300;
-    //   speed = 2000;
-    // } else if (i < 3) {
-    //   offset1 = 800;
-    //   offset2 = 600;
-    //   speed = 400;
-    // } else {
-    //   offset1 = 1800;
-    //   offset2 = 600;
-    //   speed = 400;
-    // }
 
     // console.log(elementTop);
     if (windowScrollTop > elementTop - windowHeight + offset1) {
