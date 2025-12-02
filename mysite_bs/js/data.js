3ef59091b7182052952ca22d1e686c7c590e2b3d
rwl-creator <xxx696193@gmail.com>
2025-12-02 17:13:27 +0900

    1202

---
+++ mysite_bs/js/data.js
@@ -0,0 +1,39 @@
+// 카드 데이터 배열
+const datas = [
+  {
+    rank: 1,
+    poster: "https://www.dummyimage.com/300x400/000/fff&text=preparing+...",
+    title: '에코티브',
+    description: ["준비중 ..."],
+    worktime: '240h',
+    task: 'PE',
+    siteUrl: '#'
+  },
+  {
+    rank: 2,
+    poster: "https://www.dummyimage.com/300x400/000/fff&text=preparing+...",
+    title: '???',
+    description: ["준비중 ..."],
+    worktime: '240h',
+    task: 'PE',
+    siteUrl: '#'
+  },
+  {
+    rank: 3,
+    poster: "https://www.dummyimage.com/300x400/000/fff&text=preparing+...",
+    title: '팀 작업',
+    description: ["준비중 ..."],
+    worktime: '240h',
+    task: 'TE',
+    siteUrl: '#'
+  },
+  {
+    rank: 4,
+    poster: "assets/img/project4_thumb.png",
+    title: '???',
+    description: ["풀 페이지 방식", "적응형 웹 사이트", "페이지 수: 5장", "기술스택: HTML, CSS, JS(JQuery) 등"],
+    worktime: '40h',
+    task: 'PE',
+    siteUrl: 'https://410097800525331.github.io/jfs/project'
+  },
+];
